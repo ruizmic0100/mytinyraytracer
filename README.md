8367559c85a59ff729bdb2a7ee55d3c33307c873
ruizmic0100 <ruizmic0100@hotmail.com>
2023-03-09 00:21:35 -0600

    Added a sphere and background to the raytracer as part of the tutorial.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mytinyraytracer
 Computer graphics practice creating a tiny ray tracer closely following this project https://github.com/ssloy/tinyraytracer
+
+## Conversion
+
+Install ffmpeg and run,
+
+```
+$> ffmpeg -i out.ppm out.png
+```
---
+++ build/CMakeFiles/mytinytracer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mytinytracer.dir/mytinytracer.cpp.o"
+  "mytinytracer"
+  "mytinytracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mytinytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
